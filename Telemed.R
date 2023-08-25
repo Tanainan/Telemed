57415bc0513c6fbcc478d8a5273a9432a9ae84a2
Tanainan C <tanainan.chu@gmail.com>
2023-08-25 15:07:57 +0700

    2

--- Telemed.R
+++ Telemed.R
@@ -1,6 +1,8 @@
 library(readxl)
 library(tidyverse)
 library(anytime)
+library(eeptools)
+library(glue)
 
 # import data
 person <- data.frame(read_excel("~/Downloads/Telemed/43 แฟ้ม Telemed/PERSON.xlsx"))
@@ -86,15 +88,62 @@ sum(table(data_tele$PID) > 1 & table(data_tele$PID) < 4) # 91
 # more than 4
 sum(table(data_tele$PID) < 4) # 136
 
+# calculate age from BIRTH
+date_today <- Sys.Date()
+date_today
+
+data_tele$age <- NA
+data_tele$age <- age_calc(data_tele$BIRTH, date_today, units = "years")
+
+mean(data_tele$age) # 59.81989
+
+# categorize ages into groups
+data_tele <- data_tele %>%
+  mutate(age_group = case_when(
+    age >=0 & age <4 ~ "1",
+    age >=4 & age <8 ~ "2",
+    age >=8 & age <12 ~ "3",
+    age >=12 & age <16~ "4",
+    age >=16 & age <20~ "5",
+    age >=20 & age <24~ "6",
+    age >=24 & age <28~ "7",
+    age >=28 & age <32~ "8",
+    age >=32 & age <36~ "9",
+    age >=36 & age <40~ "10",
+    age >=40 & age <44~ "11",
+    age >=44 & age <48~ "12",
+    age >=48 & age <52~ "13",
+    age >=52 & age <56~ "14",
+    age >=56 & age <60~ "15",
+    age >=60 & age <64~ "16",
+    age >=64 & age <68~ "17",
+    age >=68 & age <72~ "18",
+    age >=72 & age <76~ "19",
+    age >=76 & age <80~ "20",
+    age >=80 & age <84~ "21",
+    age >=84 & age <88~ "22",
+    age >=88 & age <92~ "23",
+    age >=92 & age <96~ "24",
+    age >=96 & age <100~ "25"
+  ))
+
+# age group of visit
+table(data_tele$age_group)
+table(subset(data_tele, SEX == 1)$age_group) # male
+# 10 11 12 13 14 15 16 17 18 19 20 21 22 23  3  7  8  9
+# 21 58 27 40 15 53 52 55 39 36  3  9 22  9 19 25  1 25
+
+table(subset(data_tele, SEX == 2)$age_group) # female
+# 10  11  12  13  14  15  16  17  18  19  20  21  22  23  24   3   6   7   8   9
+# 38  43  78 102 137 175 210 173  82 137  66  86   7  14   5   9   1   1   2  14
+
+# use data_tele1 for diseases identification
 # remove NAs
 data_tele1 <- na.omit(data_tele) # 1802
-
-# sort DIAGCODE
-# create a new column for DIAGCODE
 data_tele1$code <- NA
 
 # rank the diseases
-data.frame(count=sort(table(data_tele$DIAGCODE), decreasing=TRUE))
+data.frame(count=sort(table(data_tele1$DIAGCODE), decreasing=TRUE))
 
 # 1        E789        499 Other disorders of purine and pyrimidine metabolism
 # 2         I10        498 Essential (primary) hypertension
@@ -102,4 +151,9 @@ data.frame(count=sort(table(data_tele$DIAGCODE), decreasing=TRUE))
 # 4        Z719         82 Counselling\, unspecified
 # 5        Z133         69 Special screening examination for mental and behavioural disorders
 
+# sort DIAGCODE
+# create a new column for DIAGCODE
+data_tele1$code <- substr(data_tele1$DIAGCODE, 1, 3)
 
+# rank the diseases
+data.frame(count=sort(table(data_tele1$code), decreasing=TRUE))
